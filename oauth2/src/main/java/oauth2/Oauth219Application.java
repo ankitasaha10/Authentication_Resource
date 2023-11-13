9c036586181612dce49f6e80e045c2aed0e0920d
ankitasaha101 <107093097+ankitasaha10@users.noreply.github.com>
2023-11-13 22:25:02 +0530

    Add files via upload
    generated token

---
+++ oauth2/src/main/java/oauth2/Oauth219Application.java
@@ -0,0 +1,13 @@
+package oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth219Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth219Application.class, args);
+	}
+
+}
---
+++ oauth2/src/main/java/oauth2/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+		UserDetails userDetails = User.withUsername("test@sunbasedata.com").password("Test@123")
+				.authorities("read", "write").build();
+		inMemoryUserDetailsManager.createUser(userDetails);
+
+		return inMemoryUserDetailsManager;
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.csrf().disable();
+	}
+}
